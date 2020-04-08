0968f6c819ecb5b8b06d95050c4eaea558482f46
mkpetterson <maureen.petterson@gmail.com>
2020-04-08 15:13:44 -0700

    finished initial EDA

--- src/helper.py
+++ src/helper.py
@@ -28,36 +28,51 @@ def calculate_diff(df, column, new_col_name='diff'):
     return df
 
 
-def plotter(df, x, columns):
+def plotter(df, x, columns, title=None):
     """ Simple plotter
     
     Inputs:
     pandas dataframe
     x axis : string
     columns: list of column names
+    title: string
     
     Returns:
-    Figure 
+    None
     """    
     
     num_plots = len(columns)
-    rows = math.ceil(num_plots/3)
+    rows = math.ceil(num_plots/2)
     
-    if num_plots>3:
-        fig, axes = plt.subplots(rows, 3, figsize=(24,12))
+    if num_plots>2:
+        fig, axes = plt.subplots(rows, 2, figsize=(24,12))
     else:
         fig, axes = plt.subplots(rows, num_plots, figsize=(10,4))
     
     for i, ax in enumerate(axes.flatten()):
         
         if i < num_plots:         
-            ax.plot(df[x], df[columns[i]], 'bo')
-            ax.set_title(columns[i])
+            ax.plot(df[x], df[columns[i]])
+            ax.set_title(columns[i], fontsize=12)
             ax.set_xlabel(x)
-            ax.set_ylabel(columns[i])
+            ax.set_ylabel(columns[i], fontsize=12)
         else:
             break
         
+    fig.suptitle(title, x=0.5, y=1.05,fontsize=18)    
     fig.tight_layout()
         
+    return None
+
+
+def plotter_onegraph(df, x, columns):
+    
+    fig, ax = plt.subplots()
+    for col in columns:
+        ax.plot(df[x], df[col], label=col)
+  
+    plt.legend()
+    plt.xlabel(x)
+    fig.tight_layout()
+    
     return None
